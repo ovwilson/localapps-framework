b291804d850fee6f3bff68536c2a3ff5ceb4b281
landough <orlando_wilson@msn.com>
2016-10-01 21:22:53 -0400

    Merging Webpack config

--- src/webpack.config.app.sample.js
+++ src/webpack.config.app.sample.js
@@ -1,80 +1,81 @@
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var helpers = require('./helpers');
+var webpack = require("webpack"),
+    ExtractTextPlugin = require('extract-text-webpack-plugin'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    helpers = require('./helpers');
 
 const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
 
 module.exports = {
-  devtool: 'source-map',  
-  entry: {
-    'polyfills': './src/polyfills.ts',
-    'vendor': './src/vendor.ts',
-    'app': './app-sample/main.ts'
-  },
+   
+   devtool: 'source-map',
+               
+    entry: {
+        'polyfills': './src/polyfills.ts',
+        'vendor': './src/vendor.ts',
+        'app': './app-sample/main.ts'
+    },               
 
-  output: {
-    path: helpers.root('dist'),
-    publicPath: '/dist/',
-    filename: '[name].[hash].js',
-    chunkFilename: '[id].[hash].chunk.js'
-  },
+    output: {
+        path: helpers.root('app-sample/dist'),
+        publicPath: '/app-sample/dist/',
+        filename: '[name].[hash].js',
+        chunkFilename: '[id].[hash].chunk.js'
+    },
 
-  resolve: {
-    extensions: ['', '.js', '.ts']
-  },
+    htmlLoader: {
+        minimize: false // workaround for ng2
+    },
 
-  htmlLoader: {
-    minimize: false // workaround for ng2
-  },
+    resolve: {
+        extensions: ['', '.js', '.ts']
+    },
 
-  module: {
-    loaders: [
-      {
-        test: /\.ts$/,
-        loaders: ['awesome-typescript-loader', 'angular2-template-loader']
-      },
-      {
-        test: /\.html$/,
-        loader: 'html'
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-        loader: 'file?name=assets/[name].[hash].[ext]'
-      },
-      {
-        test: /\.css$/,
-        exclude: helpers.root('src', 'app'),
-        loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
-      },
-      {
-        test: /\.css$/,
-        include: helpers.root('src', 'app'),
-        loader: 'raw'
-      }
+    module: {
+        loaders: [
+        {
+            test: /\.ts$/,
+            loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+        },
+        {
+            test: /\.html$/,
+            loader: 'html'
+        },
+        {
+            test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+            loader: 'file?name=assets/[name].[hash].[ext]'
+        },
+        {
+            test: /\.css$/,
+            exclude: helpers.root('src', 'app'),
+            loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
+        },
+        {
+            test: /\.css$/,
+            include: helpers.root('src', 'app'),
+            loader: 'raw'
+        }
+        ]
+    },
+  
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+          name: ['app', 'vendor', 'polyfills']
+        }),
+        new HtmlWebpackPlugin({
+          template: 'app-sample/index.html'
+        }),
+        new webpack.NoErrorsPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({ // https://github.com/angular/angular/issues/10618
+          mangle: {
+              keep_fnames: true
+          }
+        }),
+        new ExtractTextPlugin('[name].[hash].css'),
+        new webpack.DefinePlugin({
+          'process.env': {
+              'ENV': JSON.stringify(ENV)
+          }
+        })
     ]
-  },
-
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: ['app', 'vendor', 'polyfills']
-    }),
-
-    new HtmlWebpackPlugin({
-      template: 'app-sample/index.html'
-    }),
-    new webpack.NoErrorsPlugin(),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({ // https://github.com/angular/angular/issues/10618
-      mangle: {
-        keep_fnames: true
-      }
-    }),
-    new ExtractTextPlugin('[name].[hash].css'),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'ENV': JSON.stringify(ENV)
-      }
-    })
-  ]
 };
